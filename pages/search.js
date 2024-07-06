ca919c89d85eb2af1da1b4d5b39704e341ec806b
JiinSeok <seokjiin1073@gmail.com>
2024-07-07 02:50:28 +0900

    쿼리 사용

--- pages/search.js
+++ pages/search.js
@@ -1,12 +1,14 @@
 import { useRouter } from 'next/router';
+import SearchForm from "@/components/SearchForm";
 
 export default function Search() {
-    const { query } = useRouter().query;
+    const { keyword } = useRouter().query;
 
     return (
         <div>
             <h1>watchit</h1>
-            <p>{query} 검색 결관</p>
+            <SearchForm initialValue={keyword} />
+            <p>{keyword} 검색 결과</p>
         </div>
     );
 }
