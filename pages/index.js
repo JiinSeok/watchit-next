3b0fef39ae39d3c56114d3594574b8993371cefa
JiinSeok <seokjiin1073@gmail.com>
2024-07-09 16:34:45 +0900

    영화 전체 데이터 추가

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,32 @@ import SearchForm from '@/components/SearchForm';
 import styles from '@/styles/Home.module.css';
 import Header from '@/components/Header';
 import Container from '@/components/Container';
+import { useEffect, useState } from "react";
+import axios from "@/lib/axios";
 
 export default function Home() {
+    const [movies, setMovies] = useState([]);
+
+    async function getMovies() {
+        const response = await axios.get('/movies');
+        const nextMovies = response.data.results ?? [];
+        setMovies(nextMovies);
+    }
+
+    useEffect(() => {
+        getMovies();
+    }, []);
+
+    if (!movies) {
+        return <div>정보가 없습니다.</div>;
+    }
 
     return (
         <>
             <Header />
             <Container page>
                 <SearchForm />
-                <MovieList className={styles.movieList} />
+                <MovieList className={styles.movieList} movies={movies} />
             </Container>
         </>
     );
