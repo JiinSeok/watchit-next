3f0b903d286f070b078f49741377d8754f60ba33
JiinSeok <seokjiin1073@gmail.com>
2024-07-09 16:01:04 +0900

    템플릿 코드 추가

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,18 @@
-import Link from "next/link";
+import MovieList from '@/components/MovieList';
+import SearchForm from '@/components/SearchForm';
+import styles from '@/styles/Home.module.css';
+import Header from '@/components/Header';
+import Container from '@/components/Container';
 
 export default function Home() {
-  return (
-      <>
-        <h1>watchit</h1>
-        <ul>
-          <Link href="/movies/1">
-            1번 영화
-          </Link>
-          <Link href="/movies/2">
-            2번 영화
-          </Link>
-          <Link href="movies/3">
-            3번 영화
-          </Link>
-        </ul>
-      </>
-  );
+
+    return (
+        <>
+            <Header />
+            <Container page>
+                <SearchForm />
+                <MovieList className={styles.movieList} />
+            </Container>
+        </>
+    );
 }
