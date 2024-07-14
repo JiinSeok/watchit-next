1430a0cb6e26156ff248390362367290aaa07d5c
JiinSeok <seokjiin1073@gmail.com>
2024-07-14 17:33:32 +0900

    폰트 Noto Sans KR 적용

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,12 @@ import "@/styles/global.css";
 import Header from "@/components/Header";
 import {ThemeProvider} from "@/lib/ThemeContext";
 import Head from "next/head";
+import { Noto_Sans_KR } from 'next/font/google';
+
+const notoSansKR = Noto_Sans_KR({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }) {
   return (
@@ -9,6 +15,9 @@ export default function App({ Component, pageProps }) {
           <Head>
             <title>Watchit</title>
             <link rel="icon" href="/favicon.ico" />
+            <style>{`
+              font-family: ${notoSansKR}.style.fontFamily, sans-serif;
+            `}</style>
           </Head>
           <ThemeProvider>
               <Header />
