b1cacfdd27ed72c6d08dff618472ae1b4e33d964
JiinSeok <seokjiin1073@gmail.com>
2024-08-27 23:28:56 +0900

    배포 재시도

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ export async function getStaticProps() {
 export default function Home({ movies }) {
   return (
     <>
+      하하
       <SearchForm />
       <MovieList className={styles.movieList} movies={movies} />
     </>
